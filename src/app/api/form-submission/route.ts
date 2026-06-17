fe7d711b23111ce66e56091272183069f13978ec
shenoac <shenoachee@gmail.com>
2026-06-17 20:18:20 +0200

    Change Status

--- src/app/api/form-submission/route.ts
+++ src/app/api/form-submission/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
     .insert({
       project_id: project.id,
       owner: body.owner,
-      status: body.status || "matching",
+      status: body.status || "new request",
     });
 
   if (insertError) {
